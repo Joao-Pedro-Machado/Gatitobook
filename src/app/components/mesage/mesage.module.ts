077c9fed5082b5e526c9429da9b6db1a11ed240a
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-03-28 14:35:44 -0300

    Creating mesage component

---
+++ src/app/components/mesage/mesage.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MesageComponent } from './mesage.component';
+
+
+
+@NgModule({
+  declarations: [
+    MesageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MesageComponent
+  ]
+})
+export class MesageModule { }
