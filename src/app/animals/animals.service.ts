2d4c8c9d1ed231e33262b429796e542c6602af32
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-04-06 14:27:22 -0300

    Creaing animals service

---
+++ src/app/animals/animals.service.ts
@@ -0,0 +1,22 @@
+import { TokenService } from './../auth/token.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Animals } from './animals';
+
+const API = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimalsService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+  userList(userName: string): Observable<Animals> {
+    const token = this.tokenService.getToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animals>(`${API}/${userName}/photos`, {
+      headers,
+    });
+  }
+}
