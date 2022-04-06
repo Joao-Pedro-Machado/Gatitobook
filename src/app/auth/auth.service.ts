bd3bcab40582a00a00047badc5a2b4d4aa95f5b7
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-04-06 14:37:19 -0300

    Crating enviroment property apiURL

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,26 +1,35 @@
+import { environment } from './../../environments/environment';
 import { UserService } from './user/user.service';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 
+const API = environment.apiURL;
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private httpClient: HttpClient, private userService: UserService) { }
+  constructor(
+    private httpClient: HttpClient,
+    private userService: UserService
+  ) {}
 
   auth(user: string, password: string): Observable<HttpResponse<any>> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: user,
-      password: password
-    },
-    { observe: 'response' }
-    ).pipe(
-      tap((res) => {
-        const authToken = res.headers.get('x-access-token') ?? ''
-        this.userService.setToken(authToken)
-      })
-    )
+    return this.httpClient
+      .post(
+        `${API}/user/login`,
+        {
+          userName: user,
+          password: password,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.userService.setToken(authToken);
+        })
+      );
   }
 }
--- src/app/home/new-user/new-user.service.ts
+++ src/app/home/new-user/new-user.service.ts
@@ -1,19 +1,21 @@
+import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NewUser } from './new-user';
 
+const API = environment.apiURL;
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NewUserService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   registerNewUser(newUser: NewUser) {
-    return this.http.post('http://localhost:3000/user/signup', newUser)
+    return this.http.post(`${API}/user/signup`, newUser);
   }
 
   userAlreadyExists(userName: string) {
-    return this.http.get(`http://localhost:3000/user/exists/${userName}`)
+    return this.http.get(`${API}/user/exists/${userName}`);
   }
 }
