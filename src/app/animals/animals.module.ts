568477c457f98c02b57be821e9efd7d56697459a
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-04-06 14:13:33 -0300

    Creating card component

--- src/app/animals/animals.module.ts
+++ src/app/animals/animals.module.ts
@@ -5,15 +5,8 @@ import { AnimalsRoutingModule } from './animals-routing.module';
 import { AnimalListComponent } from './animal-list/animal-list.component';
 import { AnimalComponent } from './animal/animal.component';
 
-
 @NgModule({
-  declarations: [
-    AnimalListComponent,
-    AnimalComponent
-  ],
-  imports: [
-    CommonModule,
-    AnimalsRoutingModule
-  ]
+  declarations: [AnimalListComponent, AnimalComponent],
+  imports: [CommonModule, AnimalsRoutingModule],
 })
-export class AnimalsModule { }
+export class AnimalsModule {}
