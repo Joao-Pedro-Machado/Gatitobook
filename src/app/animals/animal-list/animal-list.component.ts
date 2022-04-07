ae8fcafded43382d70fdcc76ebbf6523edf85cb7
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-04-07 08:36:39 -0300

    Creating photo grid and photo list

--- src/app/animals/animal-list/animal-list.component.ts
+++ src/app/animals/animal-list/animal-list.component.ts
@@ -1,15 +1,27 @@
+import { UserService } from 'src/app/auth/user/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Animals } from '../animals';
+import { AnimalsService } from '../animals.service';
 
 @Component({
   selector: 'app-animal-list',
   templateUrl: './animal-list.component.html',
-  styleUrls: ['./animal-list.component.css']
+  styleUrls: ['./animal-list.component.css'],
 })
 export class AnimalListComponent implements OnInit {
+  animals!: Animals;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private animalsService: AnimalsService
+  ) {}
 
   ngOnInit(): void {
+    this.userService.getUser().subscribe((user) => {
+      const userName = user.name ?? '';
+      this.animalsService.userList(userName).subscribe((animals) => {
+        this.animals = animals;
+      });
+    });
   }
-
 }
