c007599a8b317ffebceb5675f2107e6625042f24
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-04-07 12:23:40 -0300

    Creating animal detail route service

--- src/app/animals/animal-details/animal-details.component.ts
+++ src/app/animals/animal-details/animal-details.component.ts
@@ -1,15 +1,25 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../animals';
+import { AnimalsService } from '../animals.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-animal-details',
   templateUrl: './animal-details.component.html',
-  styleUrls: ['./animal-details.component.css']
+  styleUrls: ['./animal-details.component.css'],
 })
 export class AnimalDetailsComponent implements OnInit {
+  animalId!: number;
+  animal$!: Observable<Animal>;
 
-  constructor() { }
+  constructor(
+    private animalsService: AnimalsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params['animalId'];
+    this.animal$ = this.animalsService.findById(this.animalId);
   }
-
 }
--- src/app/animals/animals.service.ts
+++ src/app/animals/animals.service.ts
@@ -3,7 +3,7 @@ import { TokenService } from './../auth/token.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Animals } from './animals';
+import { Animal, Animals } from './animals';
 
 const API = environment.apiURL;
 
@@ -19,5 +19,11 @@ export class AnimalsService {
     return this.http.get<Animals>(`${API}/${userName}/photos`, {
       headers,
     });
+  }
+
+  findById(id: number): Observable<Animal> {
+    const token = this.tokenService.getToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animal>(`${API}/photos/${id}`, { headers });
   }
 }
