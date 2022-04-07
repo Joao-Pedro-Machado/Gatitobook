2d009beed51a93c65232b9a1c18e0278f7f4d9b3
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-04-07 14:01:25 -0300

    Creating like and remove methods on service

--- src/app/animals/animals.service.ts
+++ src/app/animals/animals.service.ts
@@ -2,10 +2,11 @@ import { environment } from './../../environments/environment';
 import { TokenService } from './../auth/token.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, mapTo, Observable, of, throwError } from 'rxjs';
 import { Animal, Animals } from './animals';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +20,20 @@ export class AnimalsService {
 
   findById(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  removePhotoById(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  like(id: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${id}/likes`, {}, { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 }
