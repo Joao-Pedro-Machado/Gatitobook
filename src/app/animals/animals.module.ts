d1f2bc2490816d1dfa6dc342fefe0b5abccf558f
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-04-11 11:56:12 -0300

    Creating template for comment component

--- src/app/animals/animals.module.ts
+++ src/app/animals/animals.module.ts
@@ -8,6 +8,8 @@ import { CardModule } from '../components/card/card.module';
 import { AnimalsPhotoGridComponent } from './animals-photo-grid/animals-photo-grid.component';
 import { AnimalDetailsComponent } from './animal-details/animal-details.component';
 import { CommentsComponent } from './animal-details/comments/comments.component';
+import { MessageModule } from '../components/message/message.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,12 @@ import { CommentsComponent } from './animal-details/comments/comments.component'
     AnimalDetailsComponent,
     CommentsComponent,
   ],
-  imports: [CommonModule, AnimalsRoutingModule, CardModule],
+  imports: [
+    CommonModule,
+    AnimalsRoutingModule,
+    CardModule,
+    MessageModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AnimalsModule {}
