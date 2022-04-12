6bc6192ff813f7d9fcb27304ff924ad914042790
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-04-12 15:16:44 -0300

    Creating new photo methods

--- src/app/animals/new-animal/new-animal.component.ts
+++ src/app/animals/new-animal/new-animal.component.ts
@@ -1,5 +1,9 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
+import { AnimalsService } from '../animals.service';
 
 @Component({
   selector: 'app-new-animal',
@@ -12,11 +16,43 @@ export class NewAnimalComponent implements OnInit {
   preview!: string;
   conclusion = 0;
 
-  constructor() {}
+  constructor(
+    private animalsService: AnimalsService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.animalForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
+  }
 
-  upload() {}
+  upload() {
+    const allowComments = this.animalForm.get('allowComments')?.value ?? false;
+    const description = this.animalForm.get('descripton')?.value ?? '';
 
-  saveFile(file: any) {}
+    this.animalsService
+      .upload(description, allowComments, this.file)
+      .pipe(finalize(() => this.router.navigate(['animals'])))
+      .subscribe(
+        (event: HttpEvent<any>) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            const total = event.total ?? 1;
+            this.conclusion = Math.round(100 * (event.loaded / total));
+          }
+        },
+        (error) => console.log(error)
+      );
+  }
+
+  saveFile(file: any): void {
+    const [archive] = file?.files;
+    this.file = archive;
+    const reader = new FileReader();
+    reader.onload = (event: any) => (this.preview = event.target.result);
+    reader.readAsDataURL(file);
+  }
 }
